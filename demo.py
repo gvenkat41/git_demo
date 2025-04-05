0fb2ae77757442013ef3fffe0fc9a5ecac6142b2
Venkat Bond <gvenkatyadav888@gmail.com>
2025-04-05 10:49:32 +0530

    added temp.py and demo.py

---
+++ demo.py
@@ -0,0 +1,5 @@
+from temp import sum
+
+if __name__ == '__main__':
+    x = sum(7, 8)
+    print(x)
---
+++ temp.py
@@ -0,0 +1,6 @@
+def sum(a, b):
+    return a + b
+
+if __name__ == '__main__':
+    x = sum(4, 5)
+    print(x)
